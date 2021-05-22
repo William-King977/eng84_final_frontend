35de4b46b9f9e428440b07089563632218d88c9b
conjectures <alexis.othonos@gmail.com>
2021-05-22 22:30:09 +0100

    refactor teams, fixed background, added favicon

--- app.py
+++ app.py
@@ -6,11 +6,79 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	# with open('static/ItJobsWatchTop30.csv', newline='') as f:
-	#     reader = csv.reader(f)
-	#     top30_jobs_list = list(reader)
-	# return render_template("home.html", job_list=top30_jobs_list)
-    return render_template('home.html')
+    # with open('static/ItJobsWatchTop30.csv', newline='') as f:
+    #     reader = csv.reader(f)
+    #     top30_jobs_list = list(reader)
+    # return render_template("home.html", job_list=top30_jobs_list)
+    # Include team member names and teams
+    context = {
+            "front": [
+                {
+                    "name": "Alexis Othonos",
+                    "src": "static/team/alexis.jpg",
+                    "role": "Frontend Scrum master",
+                },
+                {
+                    "name": "Ula Ziolko",
+                    "src": "static/team/ula.jpg",
+                    "role": "Frontend",
+                },
+                {
+                    "name": "Oleg Negruta",
+                    "src": "/static/team/oleg.jpg",
+                    "role": "Frontend",
+                },
+
+                {
+                    "name": "William King",
+                    "src": "static/team/will.jpg",
+                    "role": "Frontend",
+                },
+            ],
+            "auto": [
+                {
+                    "name": "Ben Ranson",
+                    "src": "static/team/ben.jpg",
+                    "role": "Automation Scrum master",
+                },
+                {
+                    "name": "Saverio Cutrupi",
+                    "src": "static/team/savi.jpg",
+                    "role": "Automation ",
+                },
+                {
+                    "name": "Isobel Fitt-Conway",
+                    "src": "static/team/isobel.jpg",
+                    "role": "Automation",
+                },
+                {
+                    "name": "Andrew Asare",
+                    "src": "static/team/andrew.jpg",
+                    "role": "Automation",
+                },
+
+                ],
+            "test": [
+                {
+                    "name": "Arun Panesar",
+                    "src": "static/team/arun.jpg",
+                    "role": "Testing Scrum Master",
+                },
+                {
+                    "name": "Jordan Clarke",
+                    "src": "static/team/jordan.jpg",
+                    "role": "Testing Engineer",
+                },
+                {
+                    "name": "Jose Torres",
+                    "src": "static/team/jose.jpg",
+                    "role": "Testing Engineer",
+                },
+
+                ]
+            }
+
+    return render_template('home.html', context=context )
 
 
 @app.route("/meet_team")
@@ -26,12 +94,12 @@ def data():
 # Remove once Top 30 Jobs table has been added to base.html
 @app.route("/top30-test")
 def collect_data():
-	# Import the CSV and convert it into a list
-	with open('static/ItJobsWatchTop30.csv', newline='') as f:
-	    reader = csv.reader(f)
-	    top30_jobs_list = list(reader)
+    # Import the CSV and convert it into a list
+    with open('static/ItJobsWatchTop30.csv', newline='') as f:
+        reader = csv.reader(f)
+        top30_jobs_list = list(reader)
 
-	return render_template("top30-test.html", job_list=top30_jobs_list)
+    return render_template("top30-test.html", job_list=top30_jobs_list)
 
 
 if __name__ == "__main__":
